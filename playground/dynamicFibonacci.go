43fd9a57e0b2f4cf78ad179e82667858d21e6e18
Ed Bacher <ed.bacher@gmail.com>
2023-07-08 17:29:21 -0400

    working version with debug print statements

--- playground/dynamicFibonacci.go
+++ playground/dynamicFibonacci.go
@@ -4,10 +4,12 @@ package main
 
 import (
     "strconv"
-    "fmt")
+    "fmt"
+)
 
 func main() {
-    //fibonacciTest()
+    // Compare non-dynamic and dynamic
+    fibonacciTest()
     dynamicFibTest()
 }
 
@@ -35,9 +37,22 @@ func fibonacciOnTheFly(N int64) int64 {
     if int64(len(fibonacciValues)) > N {
         return fibonacciValues[N]
     }
+    fmt.Println("length of fibonacciValues:", len(fibonacciValues))
+    // we need to calculate some values now
+    fmt.Println("adding to fibonacciValues starting at", len(fibonacciValues))
+    fmt.Println("fibValues:",fibonacciValues)
+    for i := int64(len(fibonacciValues))-1; i < N; i++ {
+        fmt.Println("calculating fib", i+1, " by adding fibs ", i, i-1)
+        fibonacciValues = append(fibonacciValues, 
+            fibonacciOnTheFly(i) + fibonacciOnTheFly(i-1))
+        if fibonacciValues[i] < 0 {
+            fmt.Println("ERROR: int64 overflow at N =", i)
+            return -1
+        }
+        fmt.Println("fibValues:",fibonacciValues)
+    }
     
-    fibonacciValues = append(fibonacciValues, fibonacci(N-1) + fibonacci(N-2))
-    
+    fmt.Println("N:", N)
     return fibonacciValues[N]
 }
 
@@ -68,6 +83,7 @@ var fCount int
 
 // Tests dynamic fibonacci approaches (not all are implemented here).
 func dynamicFibTest() {
+    fCount = 0
     // Fill-on-the-fly.
     fibonacciValues = make([]int64, 2)
     fibonacciValues[0] = 0
@@ -77,6 +93,7 @@ func dynamicFibTest() {
     //initialize_slice()
 
     for {
+        fCount = 0
         // Get n as a string.
         var n_string string
         fmt.Printf("N: ")
@@ -92,6 +109,9 @@ func dynamicFibTest() {
         fmt.Printf("fibonacciOnTheFly(%d) = %d\n", n, fibonacciOnTheFly(n))
         //fmt.Printf("fibonacci_prefilled(%d) = %d\n", n, fibonacci_prefilled(n))
         //fmt.Printf("fibonacci_bottom_up(%d) = %d\n", n, fibonacci_bottom_up(n))
+        fmt.Println(fCount, "calls to fibonacciOnTheFly().")
+        fmt.Println()
+
     }
 
     // Print out all memoized values just so we can see them.
@@ -118,3 +138,4 @@ func fibonacciTest() {
         fmt.Println(fCount, "calls to fibonacci().")
     }
 }
+

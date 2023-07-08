65b9ab638abb5f0886f7b630fafedfaa45b22bfc
Ed Bacher <ed.bacher@gmail.com>
2023-07-08 13:30:21 -0400

    count the number of calls to fibonacci()

--- recursion/fibonacci.go
+++ recursion/fibonacci.go
@@ -12,6 +12,8 @@ func main() {
 
 // Calculates the Fibonacci number for N and returns it.
 func fibonacci(N int64) int64 {
+    fCount++
+    
     // input error
     if N < 0 {
         return -1
@@ -27,8 +29,13 @@ func fibonacci(N int64) int64 {
     return fibonacci(N-1) + fibonacci(N-2)
 }
 
+// Keep track of calls to fibonacci().
+var fCount int 
+
+
 func fibonacciTest() {
     for {
+        fCount = 0
         // Get n as a string.
         var n_string string
         fmt.Printf("N: ")
@@ -40,5 +47,6 @@ func fibonacciTest() {
         // Convert to int and calculate the Fibonacci number.
         n, _ := strconv.ParseInt(n_string, 10, 64)
         fmt.Printf("fibonacci(%d) = %d\n", n, fibonacci(n))
+        fmt.Println(fCount, "calls to fibonacci().")
     }
 }
